4459d08edab8cc245df3a9163aa96aee46292d78
AWinter <thumbspurcell@gmail+universityofutah.com>
2023-04-21 21:54:33 -0600

    added the functionality to click buttons and automatically update weather data

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,7 @@ init();
 function init() {
   populateSidebar(sidebar);
   populateCards();
-  var geo = runGeoCodeAPI(cities[0]);
+  runGeoCodeAPI(cities[0]);
 }
 // this function populates the side bar
 function populateSidebar(sidebar) {
@@ -27,6 +27,9 @@ function populateSidebar(sidebar) {
     button.value = cities[i];
     button.style.display = "block";
     button.style.margin = "10px";
+    button.addEventListener("click", function () {
+      runGeoCodeAPI(this.value);
+    });
   }
 }
 //creates all of the smaller cards that will contain the 5 day forcast
@@ -35,6 +38,7 @@ function populateCards() {
   for (i = 0; i < 5; i++) {
     let card = document.createElement("li");
     fiveul.appendChild(card);
+    card.className = "card";
     let date = document.createElement("h2");
     card.appendChild(date);
     date.innerHTML = "date";
@@ -56,6 +60,7 @@ async function runGeoCodeAPI(city) {
   const result = await fetch(url);
   result.json().then((json) => {
     getWeatherData(json);
+    getFiveDayForcast(json);
   });
 }
 
@@ -65,16 +70,46 @@ async function getWeatherData(response) {
   let lon = response[0].lon;
   let name = response[0].name;
   let element = document.querySelectorAll(".internal-element");
-  console.log(element);
   const apiToken = "cab88f39e9ecaaf152f3f6cf6b68c329";
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiToken}&units=imperial`;
   const result = await fetch(url);
   result.json().then((json) => {
-    console.log(json);
     populateElement(json, element, name, 0);
   });
 }
 
+async function getFiveDayForcast(response) {
+  let lat = response[0].lat;
+  let lon = response[0].lon;
+  const apiToken = "cab88f39e9ecaaf152f3f6cf6b68c329";
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,hourly,minutely,alerts&units=imperial&appid=${apiToken}`;
+  const result = await fetch(url);
+  result.json().then((json) => {
+    populateFiveCards(json);
+  });
+}
+function populateFiveCards(response) {
+  console.log("populatedfivecards");
+  let fiveul = document.querySelectorAll(".card");
+  console.log(fiveul);
+  for (i = 0; i < fiveul.length; i++) {
+    let children = fiveul[i].childNodes;
+    children[0].innerHTML = getDate(i + 1);
+    children[1].innerHTML = "🌧";
+    children[2].innerHTML = response.daily[i].temp.day + "°F";
+    children[3].innerHTML = response.daily[i].wind_speed + "mph";
+    children[4].innerHTML = response.daily[i].humidity + "%";
+  }
+}
+
+function populateElement(response, element, name, day) {
+  let date = getDate(day);
+  let title = `${name} ${date} 😀`;
+  element[0].innerHTML = title;
+  element[1].innerHTML = response.main.temp + "°F";
+  element[2].innerHTML = response.wind.speed + "mph";
+  element[3].innerHTML = response.main.humidity + "%";
+}
 //here is a function to get the date courtesy of https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
 //I also want to say that I also do understand this code and what its doing and haven't just copy pasted it in.
 function getDate(num) {
@@ -84,14 +119,4 @@ function getDate(num) {
   let year = today.getFullYear();
   let returnString = month + "/" + day + "/" + year;
   return returnString;
-}
-function getFiveDayFormat(response) {}
-
-function populateElement(response, element, name, day) {
-  let date = getDate(day);
-  let title = `${name} ${date} 😀`;
-  element[0].innerHTML = title;
-  element[1].innerHTML = response.main.temp + "°F";
-  element[2].innerHTML = response.wind.speed + "mph";
-  element[3].innerHTML = response.main.humidity + "%";
 }
